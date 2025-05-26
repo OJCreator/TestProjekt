4064b8a0fd601e27cd25fbd5d7037629953c386a
Ole Jansen <ole.jansen@gmx.de>
2025-05-26 08:12:26 +0200

    Initial commit

---
+++ Klasse.java
@@ -0,0 +1,24 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Klasse.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Klasse
+{
+    // Attribute
+
+    /**
+     * Konstruktor für Objekte der Klasse Klasse
+     */
+    public Klasse()
+    {
+    }
+
+    // weitere Methoden
+
+
+
+
+}
